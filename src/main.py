1b6dfb8519af3235d1215363022b74475bca7f11
hextim <aphex580@gmail.com>
2026-07-27 20:13:08 +0300

    first working version

--- src/main.py
+++ src/main.py
@@ -1,27 +1,41 @@
 import subprocess
+import sys
 import json
 
 # checking config
-with open("config.json", "r", encoding="utf-8") as file:
-    config_data = json.load(file) # converting the config content into a standard python dictionary
+try:
+    with open("config.json", "r", encoding="utf-8") as file:
+        config_data = json.load(file) # converting the config content into a standard python dictionary
+except Exception as err:
+    sys.exit("ERROR: failed to open the configuration file")
 
+# transferring data from the config to standard variables for convenience
 logo = config_data["logo"]
+delay_logo = config_data["delay_logo"]
+delay_text = config_data["delay_text"]
 
-# ASCII text generation (the figlet utility is available exclusively on Linux distributions)
-text = subprocess.check_output("figlet linux", shell=True, text=True).splitlines()
+try:
+    text = subprocess.check_output("figlet linux", shell=True, text=True, stderr=subprocess.STDOUT).splitlines() # ASCII text generation (the figlet utility is available exclusively on Linux distributions)
+except subprocess.CalledProcessError:
+    sys.exit("ERROR: figlet command not found")
 
 # list cursors
 logo_index = 0
 text_index = 0
 
-# comparison of object sizes
-for i in range(max(len(logo) + config_data["delay_logo"], len(text) + config_data["delay_text"])):
+max_logo_width = len(max(logo, key=len)) if logo else 0 # we find the maximum width of the ASCII logo
+total_lines = max(len(logo) + delay_logo, len(text) + delay_text) # total number of lines for image display
 
-    # image output
-    if (len(logo)+config_data["delay_logo"]-2) <= i >= config_data["delay_logo"]:
-        print(logo[logo_index] + (" " * len(max(logo, key=len))), end=""); logo_index += 1
+# image output
+for i in range(total_lines):
+    if delay_logo <= i < delay_logo + len(logo):
+        print(logo[logo_index] + " " * (max_logo_width - len(logo[logo_index])), end="")
+        logo_index += 1
     else:
-        print(" " * len(max(logo, key=len)), end="")
-    if (len(text)+config_data["delay_text"]-2) <= i >= config_data["delay_logo"]:
-        print(text[text_index]); text_index += 1
-    print(i)
+        print(" " * max_logo_width, end="")
+
+    if delay_text <= i < delay_text + len(text):
+        print(text[text_index])
+        text_index += 1
+    else:
+        print()
