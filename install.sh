1282fac49fc108c61d696912404ed3465fb24eee
hextim <aphex580@gmail.com>
2026-07-27 21:00:00 +0300

    add install script

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+repository="https://raw.githubusercontent.com/hextim/console-ascii-startup-text/main/install.sh"
+
+mkdir -p ~/.local/share/cast/ ~/.config/cast/ ~/.local/bin/
+
+curl -sSL "$repository/main.py" -o ~/.local/share/cast/main.py
+curl -sSL "$repository/config.json" -o ~/.config/cast/config.json
+
+echo "python3 ~/.local/share/cast/main.py" > "~/.local/bin/cast"
+chmod +x "~/.local/bin/cast"
+
+if ! grep -q "cast" "~/.bashrc" 2>/dev/null; then
+  echo 'cast' >> "~/.bashrc"
+fi
+
+if ! grep -q "cast" "~/.zshrc" 2>/dev/null; then
+  echo 'cast' >> "~/.zshrc"
+fi
+
+echo "The installation was successful!"
