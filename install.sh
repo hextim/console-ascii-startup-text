e677ad230a5622ae52fc853c57bdec40119c86b8
hextim <aphex580@gmail.com>
2026-07-27 21:39:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ mkdir -p ~/.local/share/cast/ ~/.config/cast/ ~/.local/bin/
 curl -sSL "$repository/main.py" -o ~/.local/share/cast/main.py
 curl -sSL "$repository/config.json" -o ~/.config/cast/config.json
 
-echo "python3 ~/.local/share/cast/main.py" > "~/.local/bin/cast"
-chmod +x "~/.local/bin/cast"
+echo "python3 ~/.local/share/cast/main.py" > ~/.local/bin/cast
+chmod +x ~/.local/bin/cast
 
 if ! grep -q "cast" "$HOME/.bashrc" 2>/dev/null; then
   echo 'cast' >> "$HOME/.bashrc"
