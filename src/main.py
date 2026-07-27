56eb1e08b088a1f3404eefa10fa9ab92c883caad
hextim <aphex580@gmail.com>
2026-07-27 17:49:56 +0300

    published the current NON-WORKING code

--- src/main.py
+++ src/main.py
@@ -1,35 +1,27 @@
-# the code is not functional yet
-
 import subprocess
 import json
-
-# example logo
-# thanks to Joan G. Stark (Spunk) for the logo
-ascii_logo = [
-    "    .---.",
-    "   /     \\",
-    "   \\.@-@./",
-    "   /`\\_/`\\",
-    "  //  _  \\\\",
-    " | \\     )|_",
-    "/`\\_`>  <_/ \\",
-    "\\__/'---'\\__/"
-]
-
-# ASCII text generation (the figlet utility is available exclusively on Linux distributions)
-ascii_text = subprocess.check_output("figlet linux", shell=True, text=True).splitlines()
 
 # checking config
 with open("config.json", "r", encoding="utf-8") as file:
     config_data = json.load(file) # converting the config content into a standard python dictionary
+
+logo = config_data["logo"]
+
+# ASCII text generation (the figlet utility is available exclusively on Linux distributions)
+text = subprocess.check_output("figlet linux", shell=True, text=True).splitlines()
 
 # list cursors
 logo_index = 0
 text_index = 0
 
 # comparison of object sizes
-for i in range(max(len(ascii_logo) + config_data["delay_logo"], len(ascii_text) + config_data["delay_text"]))
+for i in range(max(len(logo) + config_data["delay_logo"], len(text) + config_data["delay_text"])):
 
     # image output
-    (print(ascii_logo[logo_index], end=""), logo_index := logo_index + 1) if config_data["delay_logo"] >= i else " " * len(max(ascii_logo, key=len)), end="")
-    print(ascii_text[i] )
+    if (len(logo)+config_data["delay_logo"]-2) <= i >= config_data["delay_logo"]:
+        print(logo[logo_index] + (" " * len(max(logo, key=len))), end=""); logo_index += 1
+    else:
+        print(" " * len(max(logo, key=len)), end="")
+    if (len(text)+config_data["delay_text"]-2) <= i >= config_data["delay_logo"]:
+        print(text[text_index]); text_index += 1
+    print(i)
