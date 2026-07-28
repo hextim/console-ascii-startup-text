d8e3af0da717b1c4021711f4168f15246f4d1d69
hextim <aphex580@gmail.com>
2026-07-28 12:22:55 +0300

    update to installation and uninstallation scripts

--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@
 
 repository="https://raw.githubusercontent.com/hextim/console-ascii-startup-text/main/"
 
+echo "Directory creation..."
 mkdir -p ~/.local/share/cast/ ~/.config/cast/ ~/.local/bin/
 
+echo "Installing the source code..."
 curl -sSL "$repository/src/main.py" -o ~/.local/share/cast/main.py
-curl -sSL "$repository/src/config.json" -o ~/.config/cast/config.json
+curl -sSL "$repository/config.json" -o ~/.config/cast/config.json
 
+echo "Installing the utility on the system..."
 echo "python3 ~/.local/share/cast/main.py" > ~/.local/bin/cast
 chmod +x ~/.local/bin/cast
 
+echo "Adding the program to startup..."
 if ! grep -q "cast" "$HOME/.bashrc" 2>/dev/null; then
   echo 'cast' >> "$HOME/.bashrc"
 fi
@@ -19,3 +23,5 @@ if ! grep -q "cast" "$HOME/.zshrc" 2>/dev/null; then
 fi
 
 echo "The installation was successful!"
+
+read -p "Press Enter to Continue..."
--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+read -p "Are you sure you want to delete CAST? (y/N): " solution
+solution=${solution:-n}
+
+if [ "answer" != "y" ]; then
+    echo "The removal process has been cancelled"
+    exit 0
+fi
+
+rm -rf ~/.local/share/cast/
+rm -rf ~/.config/cast/
+rm -f ~/.local/bin/cast
+
+sed -i '/cast/d' ~/.bashrc 2>/dev/null
+sed -i '/cast/d' ~/.zshrc 2>/dev/null
+
+read -p "Press Enter to Continue..."
