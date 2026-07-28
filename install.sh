d4befe7af174b182cc5f48bb3756b01a477026e2
hextim <aphex580@gmail.com>
2026-07-28 10:13:28 +0300

    bug fix

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ repository="https://raw.githubusercontent.com/hextim/console-ascii-startup-text/
 
 mkdir -p ~/.local/share/cast/ ~/.config/cast/ ~/.local/bin/
 
-curl -sSL "$repository/main.py" -o ~/.local/share/cast/main.py
+curl -sSL "$repository/src/main.py" -o ~/.local/share/cast/main.py
 curl -sSL "$repository/config.json" -o ~/.config/cast/config.json
 
 echo "python3 ~/.local/share/cast/main.py" > ~/.local/bin/cast
